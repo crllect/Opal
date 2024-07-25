44ef09836236822ac255526da752172025a0ca76
crllect <crllect@proton.me>
2024-07-25 01:24:52 -0400

    dfdgghdnhgf

--- src/ts/splash.ts
+++ src/ts/splash.ts
@@ -65,10 +65,20 @@ document.addEventListener('DOMContentLoaded', () => {
 		document.removeEventListener('keydown', hideDiv);
 		document.removeEventListener('click', hideDiv);
 
-		if (!localStorage.getItem('hasVisited')) {
+		if (localStorage.getItem('hasVisited')) {
 			localStorage.setItem('hasVisited', 'true');
 
 			setTimeout(() => {
+				const overlay = document.createElement('div');
+				overlay.style.position = 'fixed';
+				overlay.style.top = '0';
+				overlay.style.left = '0';
+				overlay.style.width = '100vw';
+				overlay.style.height = '100vh';
+				overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+				overlay.style.zIndex = '999';
+				overlay.style.backdropFilter = 'blur(16px)';
+
 				const firstVisitPopup = document.createElement('div');
 				firstVisitPopup.style.position = 'fixed';
 				firstVisitPopup.style.top = '50%';
@@ -83,6 +93,69 @@ document.addEventListener('DOMContentLoaded', () => {
 				firstVisitPopup.style.borderRadius = '9px';
 				firstVisitPopup.style.width = '475px';
 				firstVisitPopup.style.height = '350px';
+				firstVisitPopup.style.zIndex = '1000';
+				firstVisitPopup.style.display = 'flex';
+				firstVisitPopup.style.flexDirection = 'column';
+				firstVisitPopup.style.alignItems = 'center';
+
+				const closeButton = document.createElement('button');
+				closeButton.style.position = 'absolute';
+				closeButton.style.top = '10px';
+				closeButton.style.left = '10px';
+				closeButton.style.width = '16px';
+				closeButton.style.height = '16px';
+				closeButton.style.background = '#A6ADC8';
+				closeButton.style.border = 'none';
+				closeButton.style.borderRadius = '100%';
+				closeButton.style.cursor = 'pointer';
+
+				const image = document.createElement('div');
+				image.style.boxShadow = '0px 2px 10px 0px rgba(0, 0, 0, 0.25)';
+				image.style.background =
+					'url("../assets/splash.png") no-repeat center center';
+				image.style.backgroundSize = 'cover';
+				image.style.width = '128px';
+				image.style.height = '128px';
+				image.style.flexShrink = '0';
+				image.style.borderRadius = '100%';
+				image.style.transform = 'translateY(0.3rem)';
+
+				const textBox = document.createElement('div');
+				textBox.style.color = '#A6ADC8';
+				textBox.style.textAlign = 'center';
+				textBox.style.fontFamily = 'Open Sans';
+				textBox.style.fontSize = '22px';
+				textBox.style.fontStyle = 'normal';
+				textBox.style.fontWeight = '400';
+				textBox.style.lineHeight = 'normal';
+				textBox.textContent =
+					'Welcome to Opal proxy, a fully featured and sleek Ultraviolet implementation';
+				textBox.style.padding = '1rem';
+
+				firstVisitPopup.addEventListener(
+					'mouseenter',
+					() => (closeButton.style.background = '#ED8796')
+				);
+				firstVisitPopup.addEventListener(
+					'mouseleave',
+					() => (closeButton.style.background = '#A6ADC8')
+				);
+
+				firstVisitPopup.appendChild(closeButton);
+				firstVisitPopup.appendChild(image);
+				firstVisitPopup.appendChild(textBox);
+
+				closeButton.addEventListener('click', () => {
+					firstVisitPopup.remove();
+					overlay.remove();
+				});
+
+				overlay.addEventListener('click', () => {
+					firstVisitPopup.remove();
+					overlay.remove();
+				});
+
+				document.body.appendChild(overlay);
 				document.body.appendChild(firstVisitPopup);
 			}, 400);
 		}
